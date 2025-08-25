50828a2d3831ff25b9852601a8137012cc105dc7
Shuvam1997 <mondal1297@gmail.com>
2025-08-25 09:38:07 +0200

    Configure src-layout packaging with package_dir and find_packages('src')

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/your-username/langgraph-email-reply-automation",
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
